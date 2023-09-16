82d7d120a91a3916667fc3ab93e6c3dcf188bd19
Jim Kriz <jim@kriz.net>
2023-09-16 15:00:54 -0400

    Python

--- Solutions/Python/app/main.py
+++ Solutions/Python/app/main.py
@@ -1,6 +1,44 @@
-def main():
-    print("Hello, World!")
+class Student:
+    def __init__(self, name, grades):
+        self.name = name
+        self.grades = grades
+
+    @property
+    def average_grade(self):
+        if not self.grades:
+            return 0
+        total = 0
+        for grade in self.grades:
+            total += grade
+        return total / len(self.grades)
+
+    @property
+    def letter_grade(self):
+        average_grade = self.average_grade
+        if average_grade >= 90:
+            return 'A'
+        elif average_grade >= 80:
+            return 'B'
+        elif average_grade >= 70:
+            return 'C'
+        elif average_grade >= 60:
+            return 'D'
+        else:
+            return 'F'
 
 
-if __name__ == "__main__":
-    main()
+class StudentTracker:
+    def __init__(self):
+        self.students = []
+
+    def add_student(self, name, grades):
+        student = Student(name, grades)
+        self.students.append(student)
+        return student
+
+    def get_students_with_letter_grade(self, letter_grade):
+        students_with_letter_grade = []
+        for student in self.students:
+            if student.letter_grade == letter_grade:
+                students_with_letter_grade.append(student)
+        return students_with_letter_grade
--- Solutions/Python/tests/test_main.py
+++ Solutions/Python/tests/test_main.py
@@ -1,23 +1,60 @@
 import unittest
-from app.main import main
-from app.utils import add, subtract
+from app.main import Student, StudentTracker
 
+class TestStudentClass(unittest.TestCase):
+    def test_calculate_average_grade(self):
+        mikey = Student('Mikey', [85, 90, 78])
+        self.assertAlmostEqual(mikey.average_grade, 84.33, 2)
 
-class TestMain(unittest.TestCase):
-    def test_main(self):
-        # Add your test cases here for the main function.
-        pass
+    def test_return_correct_letter_grade(self):
+        mikey = Student('Mikey', [95, 92, 98])
+        brand = Student('Brand', [80, 88, 76])
+        chunk = Student('Chunk', [75, 70, 68])
+        mouth = Student('Mouth', [65, 61, 58])
+        troy = Student('Troy', [55, 62, 59])
 
+        self.assertEqual(mikey.letter_grade, 'A')
+        self.assertEqual(brand.letter_grade, 'B')
+        self.assertEqual(chunk.letter_grade, 'C')
+        self.assertEqual(mouth.letter_grade, 'D')
+        self.assertEqual(troy.letter_grade, 'F')
 
-class TestUtils(unittest.TestCase):
-    def test_add(self):
-        # Add your test cases here for the add function.
-        pass
+class TestStudentTrackerClass(unittest.TestCase):
+    def test_add_student_to_tracker(self):
+        tracker = StudentTracker()
+        tracker.add_student('Andy', [92, 88, 95])
+        self.assertEqual(len(tracker.students), 1)
 
-    def test_subtract(self):
-        # Add your test cases here for the subtract function.
-        pass
+    def test_get_students_with_specific_letter_grade(self):
+        tracker = StudentTracker()
+        mikey = tracker.add_student('Mikey', [95, 92, 98])
+        chunk = tracker.add_student('Chunk', [75, 70, 68])
+        mouth = tracker.add_student('Mouth', [65, 61, 58])
+        brand = tracker.add_student('Brand', [85, 82, 88])
+        stef = tracker.add_student('Stef', [91, 89, 93])
+        troy = tracker.add_student('Troy', [55, 62, 59])
 
+        grade_a_students = tracker.get_students_with_letter_grade('A')
+        grade_b_students = tracker.get_students_with_letter_grade('B')
+        grade_c_students = tracker.get_students_with_letter_grade('C')
+        grade_d_students = tracker.get_students_with_letter_grade('D')
+        grade_f_students = tracker.get_students_with_letter_grade('F')
 
-if __name__ == "__main__":
+        self.assertEqual(len(grade_a_students), 2)
+        self.assertIn(mikey, grade_a_students)
+        self.assertIn(stef, grade_a_students)
+
+        self.assertEqual(len(grade_b_students), 1)
+        self.assertIn(brand, grade_b_students)
+
+        self.assertEqual(len(grade_c_students), 1)
+        self.assertIn(chunk, grade_c_students)
+
+        self.assertEqual(len(grade_d_students), 1)
+        self.assertIn(mouth, grade_d_students)
+
+        self.assertEqual(len(grade_f_students), 1)
+        self.assertIn(troy, grade_f_students)
+
+if __name__ == '__main__':
     unittest.main()
