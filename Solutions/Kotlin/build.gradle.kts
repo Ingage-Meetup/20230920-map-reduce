2530f1f21123aaeef7c24c91e34517e1becee321
Jim Kriz <jim@kriz.net>
2023-09-19 18:11:02 -0400

    Update gradle wrapper in kotlin

--- Solutions/Kotlin/build.gradle.kts
+++ Solutions/Kotlin/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.10"
+    kotlin("jvm") version "1.9.0"
     application
 }
 
@@ -21,5 +21,5 @@ tasks.test {
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "11"
+    kotlinOptions.jvmTarget = "18"
 }
